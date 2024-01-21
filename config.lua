02e0df739354d9afc6ef0e29992f8220e0fd81f5
azutake <azusa_takemae@dwango.co.jp>
2024-01-22 02:43:35 +0900

    commit yuka's edit

--- config.lua
+++ config.lua
@@ -6,34 +6,39 @@ Config.OpenKey = 'HOME' -- https://docs.fivem.net/docs/game-references/input-map
 Config.Toggle = true -- If this is false the scoreboard stays open only when you hold the OpenKey button, if this is true the scoreboard will be openned and closed with the OpenKey button
 
 -- Max Server Players
-Config.MaxPlayers = GetConvarInt('sv_maxclients', 48) -- It returns 48 if it cant find the Convar Int
+Config.MaxPlayers = GetConvarInt('sv_maxclients', 64) -- It returns 48 if it cant find the Convar Int
 
 -- Minimum Police for Actions
 Config.IllegalActions = {
-    ["storerobbery"] = {
+    ["houcerobbery"] = {
         minimumPolice = 2,
         busy = false,
-        label = "Store Robbery",
+        label = "空き巣"
     },
-    ["bankrobbery"] = {
+    ["drugdeliveries"] = {
         minimumPolice = 3,
         busy = false,
-        label = "Bank Robbery"
+        label = "麻薬の運び屋",
     },
-    ["jewellery"] = {
-        minimumPolice = 2,
-        busy = false,
-        label = "Jewellery"
-    },
-    ["pacific"] = {
-        minimumPolice = 5,
-        busy = false,
-        label = "Pacific Bank"
-    },
-    ["paleto"] = {
+    ["drugselling"] = {
         minimumPolice = 4,
         busy = false,
-        label = "Paleto Bay Bank"
+        label = "麻薬の手売り"
+    },
+    ["storerobbery"] = {
+        minimumPolice = 5,
+        busy = false,
+        label = "コンビニ強盗"
+    },
+    ["jewelery"] = {
+        minimumPolice = 6,
+        busy = false,
+        label = "宝石強盗"
+    },
+    ["bank"] = {
+        minimumPolice = 6,
+        busy = false,
+        label = "銀行強盗"
     }
 }
 
